d5984f300a26dfab0bcd6bf0c44a8e3777785b87
skysilver <skysilver.da@gmail.com>
2017-09-29 23:05:06 +0300

    Create mipacket.class.php
    Функции шифрования и работы с сетевыми пакетами перенесены в отдельный класс miPacket

---
+++ mipacket.class.php
@@ -0,0 +1,183 @@
+<?php
+/* 
+*	Класс для работы с сетевыми udp-пакетами по протоколу miIO.
+*	
+*	+ генерация ключа и вектора инициализации из токена
+*	+ расшифровка
+*	+ шифрование
+*	+ разбор udp-пакета
+*	+ формирование udp-пакета
+*
+*	https://github.com/OpenMiHome/mihome-binary-protocol
+*
+*	Copyright (C) 2017 Agaphonov Dmitri aka skysilver [mailto:skysilver.da@gmail.com]
+*/
+
+class miPacket {
+	
+	private $magic = '2131';
+	private $length = '';
+	private $unknown1 = '00000000';
+	private $devicetype = '';
+	private $serial = '';
+	private $ts = '';
+	private $checksum = '';
+	public $data = '';
+	
+	private $token = '';
+	private $key = '';
+	private $iv = '';
+	
+	private $hello = '21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+	
+	
+	/*
+		Сохранение токена.
+	*/
+	
+	public function setToken($token) {
+	
+		if ($this->verifyToken($token)) {
+			$this->token = $token;
+			$this->getKeyIv();
+			return true;
+		} else return false;
+	 
+	}
+	
+	/*
+		Вывод токена.
+	*/
+	
+	public function getToken() {
+	
+		return $this->token;
+	 
+	}
+	
+	/*
+		Проверка длины токена.
+	*/
+	
+	private function verifyToken($token) {
+	
+		if (strlen($token) == 32) return true;
+		else return false;
+	 
+	}
+	
+	/*
+		Вычисление ключа шифрования и вектора первичной инициализации на основе токена.
+	*/
+	
+	private function getKeyIv() {
+		
+		$this->key = md5(hex2bin($this->token));
+		$this->iv = md5(hex2bin($this->key.$this->token));
+	
+	}
+	
+	/*
+		Шифрование данных.
+	*/
+	
+	public function encryptData($data) {
+
+		if ($this->verifyToken($this->token)) {
+			return bin2hex(openssl_encrypt($data, 'AES-128-CBC', hex2bin($this->key), OPENSSL_RAW_DATA, hex2bin($this->iv)));
+		} else return false;
+	
+	}
+	
+	/*
+		Расшифровка данных.
+	*/
+	
+	public function decryptData($data) {
+		
+		if ($this->verifyToken($this->token)) {
+			return openssl_decrypt(hex2bin($data), 'AES-128-CBC', hex2bin($this->key), OPENSSL_RAW_DATA, hex2bin($this->iv));
+		} else return false;
+	
+	}
+	
+	/*
+		Формирование hello-пакета для процедуры рукопожатия (handshake).
+	*/
+	
+	public function getHello() {
+		
+		return $this->hello;
+		
+	}
+	
+	/*
+		Формирование пакета.
+	*/
+	
+	public function getRaw($cmd) {
+		
+		$this->data = $this->encryptData($cmd);
+	
+		$this->length = sprintf('%04x', (int)strlen($this->data)/2 + 32);
+			
+		$packet = $this->magic.$this->length.$this->unknown1.$this->devicetype.$this->serial.$this->ts.$this->token.$this->data;
+		
+		$packet = $this->magic.$this->length.$this->unknown1.$this->devicetype.$this->serial.$this->ts.md5(hex2bin($packet)).$this->data;
+		
+		return $packet;
+		
+	}
+	
+	/*
+		Разбор пакета по полям.
+	*/
+	
+	public function setRaw($msg) {
+		
+		$this->magic = substr($msg, 0, 4);
+		$this->length = substr($msg, 4, 4);
+		$this->unknown1 = substr($msg, 8, 8);
+		$this->devicetype = substr($msg, 16, 4);
+		$this->serial = substr($msg, 20, 4);
+		$this->ts = substr($msg, 24, 8);
+		$this->checksum = substr($msg, 32, 32);
+		
+		if ($this->length == '0020') {
+			$this->setToken(substr($msg, 32, 32));
+		} else {
+			$data_length = strlen($msg) - 64;
+			if ($data_length > 0) {
+				$this->data = substr($msg, 64, $data_length);
+			}
+		}
+		
+	}
+	
+	/*
+		Вывод заголовка пакета.
+	*/
+	
+	public function printHead() {
+		
+		echo 'magic: ' . $this->magic . PHP_EOL;
+		echo 'length: ' . $this->length . ' --> ' . hexdec($this->length) . ' байт' . PHP_EOL;
+		echo 'unknown1: ' . $this->unknown1 . PHP_EOL;
+		echo 'devicetype: ' . $this->devicetype . PHP_EOL;
+		echo 'serial: ' . $this->serial . PHP_EOL;
+		echo 'ts: ' . $this->ts . ' --> ' . hexdec($this->ts) . ' секунд'. PHP_EOL;
+		echo 'checksum: ' . $this->checksum . PHP_EOL;
+	
+	}
+	
+	/*
+		Вывод полей пакета и данных.
+	*/
+	
+	public function printPacket() {
+		
+		$this->printHead();
+		echo 'data: ' . $this->data . PHP_EOL;
+	
+	}
+}
