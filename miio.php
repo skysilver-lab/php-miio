e2f7f7dbd1955235645b2fbe3130bf08174e65cb
skysilver <skysilver.da@gmail.com>
2017-10-03 03:21:54 +0300

    Update miio.php
    Класс miio.class.php

--- miio.php
+++ miio.php
@@ -5,49 +5,43 @@
 *	Copyright (C) 2017 Agaphonov Dmitri aka skysilver [mailto:skysilver.da@gmail.com]
 */
 
-require('mipacket.class.php');
+require('miio.class.php');
 
-$device_ip = '192.168.1.47';
+$ip = '192.168.1.47'; // Адрес устройства.
+$token = 'b31c928032e6a4afc898c5c8768axxxx'; // Уникальный токен устройства.
+$debug = false; // Для отладки сменить на true.
 
-const MIIO_PORT = '54321';
+echo PHP_EOL . '<----- php-miio start ----->' . PHP_EOL;
 
-// команды управления mi-устройством (примеры)
-$cmd_miioinfo = '{"id":1,"method":"miIO.info","params":[]}';
-$cmd_off = '{"id":1,"method":"set_power","params":["off"]}';
-$cmd_on =  '{"id":1,"method":"set_power","params":["on"]}';
-$cmd_status = '{"id":1,"method":"get_prop","params":["power","color_mode","bright","ct","rgb","flowing","pdo_status","hue","sat","save_state","flow_params"]}';
- 
+// Если не указывать токен, то он будет запрошен у устройства.
+$dev = new miIO($ip, null, $debug);
 
-$con = pfsockopen('udp://'.$device_ip, MIIO_PORT, $errno, $errstr, 10);
+echo PHP_EOL . "Отправляем команду на $dev->ip" . PHP_EOL;
 
-if ($con) {
-	
-	$miPacket = new miPacket();
-	
-	echo PHP_EOL;
-	echo 'Отправлен hello-пакет размером ' . fwrite($con, hex2bin($miPacket->getHello())) . ' байт' . PHP_EOL;
-	$cart = fread($con, 1024);
-	echo 'Получен ответ: ' . PHP_EOL;
-	
-	$miPacket->setRaw(bin2hex($cart)); 
-	$miPacket->printPacket();
-	
-	echo 'token: ' . $miPacket->getToken() . PHP_EOL;
-	
-	$cmd = $cmd_miioinfo; //$cmd_miioinfo $cmd_off $cmd_on $cmd_status
-	
-	echo 'Отправляем команду ' . $cmd . PHP_EOL;
-	echo 'Отправлен пакет размером ' . fwrite($con, hex2bin($miPacket->getRaw($cmd))) . ' байт' . PHP_EOL;
-	$cart = fread($con, 1024);
-	echo 'Получен ответ: ' . PHP_EOL;
-		
-	$miPacket->setRaw(bin2hex($cart));
-	$miPacket->printPacket();
-
-	$data_dec = $miPacket->decryptData($miPacket->data);	
-	
-	echo 'Расшифрованные данные: ' . $data_dec . PHP_EOL;
-	echo PHP_EOL;
-	
-	fclose($con);
+if ($dev->msgSendRcv('miIO.info', '[]') == true) {
+	echo "Устройство $dev->ip доступно и ответило:" . PHP_EOL;
+	echo $dev->data . PHP_EOL;
+} else {
+	echo "Устройство $dev->ip не доступно или не отвечает." . PHP_EOL;
 }
+
+sleep(2);
+
+if ($dev->msgSendRcvRaw('{"id":1,"method":"toggle","params":[]}') == true) {
+	echo "Устройство $dev->ip доступно и ответило:" . PHP_EOL;
+	echo $dev->data . PHP_EOL;
+} else {
+	echo "Устройство $dev->ip не доступно или не отвечает." . PHP_EOL;
+}
+
+sleep(2);
+
+if ($dev->getInfo() == true) {
+	echo "Устройство $dev->ip доступно и ответило:" . PHP_EOL;
+	echo $dev->data . PHP_EOL;
+} else {
+	echo "Устройство $dev->ip не доступно или не отвечает." . PHP_EOL;
+}
+
+echo PHP_EOL . '<-----  php-miio end  ----->' . PHP_EOL;
+echo PHP_EOL;
