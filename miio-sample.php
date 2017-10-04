a9ebd5f8ddf639bfb67286955b5b75617a8d309a
skysilver <skysilver.da@gmail.com>
2017-10-05 01:07:18 +0300

    Update and rename miio.php to miio-sample.php
    Пример работы с функциями класса miIO.

--- miio-sample.php
+++ miio-sample.php
@@ -1,19 +1,43 @@
 <?php
 /* 
-*	Реализация взаимодействия с устройствами из экосистемы xiaomi по протоколу miIO.
+*	Реализация взаимодействия с wifi-устройствами из экосистемы xiaomi по протоколу miIO.
+*
+*	Пример работы с функциями класса miIO.
 *
 *	Copyright (C) 2017 Agaphonov Dmitri aka skysilver [mailto:skysilver.da@gmail.com]
 */
 
 require('miio.class.php');
 
-$ip = '192.168.1.47'; // Адрес устройства.
-$token = 'b31c928032e6a4afc898c5c8768axxxx'; // Уникальный токен устройства.
-$debug = false; // Для отладки сменить на true.
+$ip = '192.168.1.47';
+$token = 'b31c928032e6a4afc898c5c8768axxxx';
+$debug = false;
 
-echo PHP_EOL . '<----- php-miio start ----->' . PHP_EOL;
+echo PHP_EOL . '<----- php-miio start ----->' . PHP_EOL . PHP_EOL;
 
-// Если не указывать токен, то он будет запрошен у устройства.
+$dev = new miIO(null, null, $debug);
+
+if ($dev->discover() == true) {
+	echo 'Поиск выполнен.' . PHP_EOL;
+	$devices = json_decode($dev->data);
+	$count = count($devices->devices);
+	echo "Найдено $count устройств." . PHP_EOL;
+	foreach($devices->devices as $dev) {
+		$devprop = json_decode($dev);
+		echo 	' IP ' . 		$devprop->ip . 
+				' DevType ' . 	$devprop->devicetype . 
+				' Serial ' . 	$devprop->serial . 
+				' Token ' . 	$devprop->token . PHP_EOL;
+		$d = new miIO($devprop->ip, $devprop->token, $debug);
+		$d->getInfo();
+		echo $d->data . PHP_EOL;
+	}
+} else {
+	echo 'Поиск выполнен. Устройство не найдено.' . PHP_EOL;
+}
+
+/*
+
 $dev = new miIO($ip, null, $debug);
 
 echo PHP_EOL . "Отправляем команду на $dev->ip" . PHP_EOL;
@@ -42,6 +66,7 @@ if ($dev->getInfo() == true) {
 } else {
 	echo "Устройство $dev->ip не доступно или не отвечает." . PHP_EOL;
 }
+*/
 
 echo PHP_EOL . '<-----  php-miio end  ----->' . PHP_EOL;
 echo PHP_EOL;
