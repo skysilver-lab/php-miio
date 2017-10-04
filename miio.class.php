e99d3830d24b7679417729933bb39926ab7562f5
skysilver <skysilver.da@gmail.com>
2017-10-05 01:10:30 +0300

    Update miio.class.php
    Поиск устройств в сети с помощью handshake.
    Небольшие доработки.

--- miio.class.php
+++ miio.class.php
@@ -1,19 +1,20 @@
 <?php
 /* 
-*	Класс для работы с устройствами из экосистемы xiaomi по протоколу miIO.
+*	Класс для работы с wifi-устройствами из экосистемы xiaomi по протоколу miIO.
 *
-	+ прием udp-пакетов из сокета
-	+ отправка udp-пакетов в сокет
-	+ процедура рукопожатия (handshake)
-	+ отправка сообщений устройству
-	+ прием ответов от устройства
-	- поиск устройств (handshake-discovery)
-	- поиск устройств (mdns-discovery)
-	
-	https://github.com/aholstenson/miio
-	https://github.com/rytilahti/python-miio
-	https://github.com/marcelrv/XiaomiRobotVacuumProtocol
-
+*	+ прием udp-пакетов из сокета
+*	+ отправка udp-пакетов в сокет
+*	+ процедура рукопожатия (handshake)
+*	+ отправка сообщений устройству
+*	+ прием ответов от устройства
+*	+ поиск устройств (handshake-discovery)
+*	
+*	ToDo: поиск устройств (mdns-discovery)
+*	
+*	https://github.com/aholstenson/miio
+*	https://github.com/rytilahti/python-miio
+*	https://github.com/marcelrv/XiaomiRobotVacuumProtocol
+*
 *	Copyright (C) 2017 Agaphonov Dmitri aka skysilver [mailto:skysilver.da@gmail.com]
 */
 
@@ -29,7 +30,7 @@ class miIO {
 	public 	$token = '';
 	public 	$debug = '';
 	public 	$send_timeout = 5;
-	public 	$disc_timeout = 10;
+	public 	$disc_timeout = 2;
 
 	public 	$_device_ts = NULL;
     public 	$_id = '1';
@@ -43,14 +44,16 @@ class miIO {
 	
 	public function __construct($ip = NULL, $token = NULL, $debug = false) {
 		
-		$this->ip = $ip;
 		$this->debug = $debug;
 		
 		$this->miPacket = new miPacket();
 		
+		if ($ip != NULL) $this->ip = $ip;
 		if ($token != NULL) $this->token = $token;
+		
 		if ($this->debug) {
-			echo "Взаимодействие с устройством IP $this->ip" . PHP_EOL;
+			if ($this->ip == NULL) echo "Поиск устройств" . PHP_EOL;
+			 else echo "Взаимодействие с устройством IP $this->ip" . PHP_EOL;
 			echo "Статус отладки [$this->debug]" . PHP_EOL;
 		}
 	}
@@ -59,11 +62,11 @@ class miIO {
 		Поиск устройства и начало сессии с ним.
 	*/
 	
-	public function Discover($ip = NULL) {
+	public function discover($ip = NULL) {
 		
 		if ($ip != NULL) {
 			//handshake
-			if ($this->debug) echo PHP_EOL . 'Проверяем доступность устройства' . PHP_EOL;
+			if ($this->debug) echo PHP_EOL . 'Проверяем доступность устройства ' . $ip . PHP_EOL;
 			if (!($sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP))) {
 				$errorcode = socket_last_error();
 				$errormsg = socket_strerror($errorcode);
@@ -79,7 +82,7 @@ class miIO {
 			
 			$helloPacket = hex2bin(HELLO_MSG);
 			
-			socket_sendto($sock, $helloPacket, strlen($helloPacket), 0, $this->ip, MIIO_PORT);
+			socket_sendto($sock, $helloPacket, strlen($helloPacket), 0, $ip, MIIO_PORT);
 						
 		    $buf = '';
 			if (@socket_recvfrom($sock, $buf, 4096, 0, $remote_ip, $remote_port) == false) {
@@ -97,7 +100,51 @@ class miIO {
 			}
 		} else {
 			//broadcast discovery
-			//TO-DO
+			if ($this->debug) echo PHP_EOL . 'Поиск доступных устройств в локальной сети (handshake discovery)' . PHP_EOL;
+
+			if (!($sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP))) {
+				$errorcode = socket_last_error();
+				$errormsg = socket_strerror($errorcode);
+				if ($this->debug) echo "Ошибка создания сокета [$errorcode] $errormsg" . PHP_EOL;
+				die("Ошибка создания сокета [$errorcode] $errormsg \n");
+			}
+			
+			if ($this->debug) echo 'Сокет успешно создан' . PHP_EOL;
+
+			socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, array("sec" => $this->disc_timeout, "usec" => 0));
+			socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, 1);
+			
+			$ip = '255.255.255.255';
+			
+			if ($this->debug) echo "Отправляем hello-пакет на $ip с таймаутом $this->disc_timeout" . PHP_EOL;
+			
+			$helloPacket = hex2bin(HELLO_MSG);
+			
+			socket_sendto($sock, $helloPacket, strlen($helloPacket), 0, $ip, MIIO_PORT);
+			
+			$buf = '';
+		    $count = 0;
+			$devinfo = array();
+			$devices = array();
+			
+			while ($ret = @socket_recvfrom($sock, $buf, 4096, 0, $remote_ip, $remote_port)) {
+				if ($buf != '') {
+					if ($this->debug) echo ($count+1) . "  Получен ответ от IP $remote_ip" . PHP_EOL;
+					$this->miPacket->msgParse(bin2hex($buf));
+					if ($this->debug) $this->miPacket->printHead();
+					$devinfo = $this->miPacket->info;
+					$devinfo += ["ip" => $remote_ip];
+					$devices[] = json_encode($devinfo);
+				}
+				$count += 1;
+			}
+			
+			if(!empty($devices)) $this->data = '{"devices":'. json_encode($devices) .'}';
+		
+			socket_close($sock);
+			
+			if ($count != 0 || !empty($this->data)) return true;
+			 else return false;
 		}
 	}
 	
@@ -107,7 +154,7 @@ class miIO {
 
 	public function socketWriteRead($msg) {
 	
-		if ($this->Discover($this->ip)) {
+		if ($this->discover($this->ip)) {
 
 			if ($this->debug) echo PHP_EOL . "Устройство $this->ip доступно" . PHP_EOL;
 
