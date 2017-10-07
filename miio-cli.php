bce763e8f7b1225c988314f1dfb10bcf6b79d14a
skysilver <skysilver.da@gmail.com>
2017-10-08 00:51:55 +0300

    Update miio-cli.php
    Добавлена возможность указать токен вручную (в случаях, когда устройство его не передает при поиске).
    Добавлена опция расшифровки пакета --decode. Для расшифровки требуется обязательно указать токен.

--- miio-cli.php
+++ miio-cli.php
@@ -8,10 +8,11 @@
 *		--discover all	поиск устройств в локальной сети и вывод информации о них
 *		--discover IP	проверка доступности конкретного устройства и вывод информации о нем
 *		--info			получить информацию об устройстве (аналог --discover IP)
-*		--sendcmd		отправить команду (д.б. заключена в одинарные кавычки
+*		--sendcmd		отправить команду (д.б. заключена в одинарные кавычки)
+*		--decode		расшифровать пакет
 *		--ip			IP-адрес устройства
 *		--bindip		IP-адрес интерфейса сервера (не обязательно, если интерфейс один)
-*		--token			токен устройства (не обязательно)
+*		--token			токен устройства
 *		--debug			включает вывод отладочной информации
 *		--help			справка по командам
 *	
@@ -22,6 +23,7 @@
 *		php miio-cli.php --ip 192.168.1.45 --info
 *		php miio-cli.php --ip 192.168.1.45 --sendcmd '{"method":"toggle",,"params":[],"id":1}'
 *		php miio-cli.php --ip 192.168.1.47 --sendcmd '{"id":1,"method":"get_prop","params":["power"]}'
+*		php miio-cli.php --token b31c928032e6a4afc898c5c8768a518f --decode 2131004000000000035afe...bea030
 *
 *	Copyright (C) 2017 Agaphonov Dmitri aka skysilver [mailto:skysilver.da@gmail.com]
 */
@@ -33,7 +35,7 @@ ini_set('display_errors', 1);
 
 $bind_ip = null;
 
-$opts = getopt('d::', array('ip:', 'token:', 'info', 'discover:', 'sendcmd:', 'decode:', 'debug', 'help', 'bindip:'));
+$opts = getopt('d::', array('ip:', 'token:', 'info', 'discover:', 'sendcmd:', 'decode:', 'debug', 'help', 'bindip:', 'decode:'));
 
 //var_dump($opts);
 
@@ -46,9 +48,10 @@ if ( empty($opts) || isset($opts['help']) ) {
 	echo '	--discover IP	проверка доступности конкретного устройства и вывод информации о нем' . PHP_EOL;
 	echo '	--info		получить информацию об устройстве (аналог --discover IP)' . PHP_EOL;
 	echo '	--sendcmd	отправить команду (д.б. заключена в одинарные кавычки' . PHP_EOL;
+	echo '	--decode	расшифровать пакет' . PHP_EOL;
 	echo '	--ip 		IP-адрес устройства' . PHP_EOL;
 	echo '	--bindip	IP-адрес интерфейса сервера (не обязательно, если интерфейс один)' . PHP_EOL;
-	echo '	--token 	токен устройства (не обязательно)' . PHP_EOL;
+	echo '	--token 	токен устройства' . PHP_EOL;
 	echo '	--debug		включает вывод отладочной информации' . PHP_EOL;
 	echo '	--help		справка по командам' . PHP_EOL;
 	echo PHP_EOL;
@@ -59,6 +62,7 @@ if ( empty($opts) || isset($opts['help']) ) {
 	echo '	php miio-cli.php --ip 192.168.1.45 --info' . PHP_EOL;
 	echo '	php miio-cli.php --ip 192.168.1.45 --sendcmd \'{"method":"toggle",,"params":[],"id":1}\'' . PHP_EOL;
 	echo '	php miio-cli.php --ip 192.168.1.47 --sendcmd \'{"id":1,"method":"get_prop","params":["power"]}\'' . PHP_EOL;
+	echo '	php miio-cli.php --token b31c928032e6a4afc898c5c8768a518f --decode 2131004000000000035afe...bea030' . PHP_EOL;
 }
 
 if ( isset($opts['debug']) ) $debug = true;
@@ -80,7 +84,8 @@ if ( isset($opts['info']) && empty($opts['ip']) ) {
 	echo 'Необходимо указать ip-адрес устройства через параметр --ip' . PHP_EOL;
 	echo '	php miio-cli.php --ip 192.168.1.45 --info' . PHP_EOL;
 } else if (isset($opts['info']) && !empty($opts['ip'])) {
-	$dev = new miIO($opts['ip'], $bind_ip, null, $debug);
+	if (isset($opts['token']) && !empty($opts['token'])) $dev = new miIO($opts['ip'], $bind_ip, $opts['token'], $debug);
+	 else $dev = new miIO($opts['ip'], $bind_ip, null, $debug);
 	if ($dev->getInfo() == true) {
 		echo 'Информация об устройстве:' . PHP_EOL;
 		echo $dev->data . PHP_EOL;
@@ -96,7 +101,8 @@ if ( isset($opts['sendcmd']) && empty($opts['ip']) ) {
 	echo 'Необходимо указать команду' . PHP_EOL;
 	echo '	php miio-cli.php --ip 192.168.1.45 --sendcmd \'{\'method\': \'get_status\', \'id\': 1}\'' . PHP_EOL;
 } else if (!empty($opts['sendcmd']) && !empty($opts['ip'])) {
-	$dev = new miIO($opts['ip'], $bind_ip, null, $debug);
+	if (isset($opts['token']) && !empty($opts['token'])) $dev = new miIO($opts['ip'], $bind_ip, $opts['token'], $debug);
+	 else $dev = new miIO($opts['ip'], $bind_ip, null, $debug);
 	if ($dev->msgSendRcvRaw($opts['sendcmd']) == true) {
 		echo "Устройство $dev->ip доступно и ответило:" . PHP_EOL;
 		echo $dev->data . PHP_EOL;
@@ -105,6 +111,16 @@ if ( isset($opts['sendcmd']) && empty($opts['ip']) ) {
 	}
 }
 
+if ( isset($opts['decode']) && empty($opts['token']) ) {
+	echo 'Необходимо указать токен устройства через параметр --token' . PHP_EOL;
+} else if (isset($opts['decode']) && !empty($opts['decode']) && isset($opts['token']) && !empty($opts['token'])) {
+	$miPacket = new miPacket();	
+	$miPacket->setToken($opts['token']);
+	$miPacket->msgParse($opts['decode']);
+	if ($debug) $miPacket->printPacket();
+	$data_dec = $miPacket->decryptData($miPacket->data);	
+	echo "Расшифрованные данные: $data_dec" . PHP_EOL;
+}
 
 function cbDiscoverAll ($bind_ip, $debug) {
 	
