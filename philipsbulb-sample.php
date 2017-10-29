755abb47212d11b4ba8bac19ded070b07e102b8d
skysilver <skysilver.da@gmail.com>
2017-10-29 22:27:14 +0300

    Update philipsbulb-sample.php
    Добавлены варианты использования автоматического и ручного формирования идентификатора сообщения.

--- philipsbulb-sample.php
+++ philipsbulb-sample.php
@@ -5,7 +5,7 @@
 *	Copyright (C) 2017 Agaphonov Dmitri aka skysilver [mailto:skysilver.da@gmail.com]
 */
 
-require('philipsbulb.class.php');
+require('./devices/philipsbulb.class.php');
 
 error_reporting(-1);
 ini_set('display_errors', 1);
@@ -15,41 +15,52 @@ $token = '40011346b40fdb540a5b6eeae1167bf5';
 $bind_ip = null;
 $debug = false;
 
+$cmd_id = 100;
+
 $bulb = new philipsBulb($ip, $bind_ip, $token, $debug);
 
-echo PHP_EOL . date('H:m:s', time());
-if($bulb->getStatus()) echo ' Статус получен.' . PHP_EOL;
-echo 'Питание: ' . $bulb->status['power'] . PHP_EOL;
-echo 'Яркость: ' . $bulb->status['bright'] . PHP_EOL;
-echo 'Цветовая температура: ' . $bulb->status['cct'] . PHP_EOL;
-echo 'Сцена: ' . $bulb->status['snm'] . PHP_EOL;
-echo 'Таймер выключения: ' . $bulb->status['dv'] . PHP_EOL;
+// По умолчанию ID всех команд равен 1.
+// Если нужно авто-формирование уникальных ID для команд из файла id.json, то вызываем функцию enableAutoMsgID().
+// Либо можно формировать ID динамически и передавать их значения в функцию явно.
+// $bulb->enableAutoMsgID();
 
-sleep(2);
-
-echo PHP_EOL . date('H:m:s', time()) . PHP_EOL;
-echo $bulb->getInfo() . PHP_EOL;
-
-sleep(2);
-
-echo PHP_EOL . date('H:m:s', time());
-if($bulb->powerOn()) echo ' Лампа включена.' . PHP_EOL;
- else echo "Лампа не включена. Ошибка: $bulb->error" . PHP_EOL;
-
-sleep(2);
-
-echo PHP_EOL . date('H:m:s', time());
-if($bulb->powerOff()) echo ' Лампа выключена.' . PHP_EOL;
- else echo "Лампа не выключена. Ошибка: $bulb->error" . PHP_EOL;
-
-sleep(2);
-
-for ($i = 1; $i < 5; $i++) {
-	echo PHP_EOL . date('H:m:s', time());
-	if($bulb->setScene($i)) echo " Включена сцена $i." . PHP_EOL;
+echo PHP_EOL . date('H:i:s', time());
+if($bulb->getStatus($cmd_id)) {
+	echo ' Статус получен.' . PHP_EOL;
+	echo 'Питание: ' . $bulb->status['power'] . PHP_EOL;
+	echo 'Яркость: ' . $bulb->status['bright'] . PHP_EOL;
+	echo 'Цветовая температура: ' . $bulb->status['cct'] . PHP_EOL;
+	echo 'Сцена: ' . $bulb->status['snm'] . PHP_EOL;
+	echo 'Таймер выключения: ' . $bulb->status['dv'] . PHP_EOL;
+	$cmd_id += 1;
 	sleep(2);
-}
 
-echo PHP_EOL . date('H:m:s', time());
-if($bulb->powerOff()) echo ' Лампа выключена.' . PHP_EOL;
- else echo "Лампа не выключена. Ошибка: $bulb->error" . PHP_EOL;
+	echo PHP_EOL . date('H:i:s', time()) . PHP_EOL;
+	echo $bulb->getInfo($cmd_id) . PHP_EOL;
+	$cmd_id += 1;
+	sleep(2);
+
+	echo PHP_EOL . date('H:i:s', time());
+	if($bulb->powerOn($cmd_id)) echo ' Лампа включена.' . PHP_EOL;
+	 else echo "Лампа не включена. Ошибка: $bulb->error" . PHP_EOL;
+	$cmd_id += 1;
+	sleep(2);
+
+	echo PHP_EOL . date('H:i:s', time());
+	if($bulb->powerOff($cmd_id)) echo ' Лампа выключена.' . PHP_EOL;
+	 else echo "Лампа не выключена. Ошибка: $bulb->error" . PHP_EOL;
+	$cmd_id += 1;
+	sleep(2);
+
+	for ($i = 1; $i < 5; $i++) {
+		echo PHP_EOL . date('H:i:s', time());
+		if($bulb->setScene($i, $cmd_id)) echo " Включена сцена $i." . PHP_EOL;
+		$cmd_id += 1;
+		sleep(2);
+	}
+
+	echo PHP_EOL . date('H:i:s', time());
+	if($bulb->powerOff($cmd_id)) echo ' Лампа выключена.' . PHP_EOL;
+	 else echo "Лампа не выключена. Ошибка: $bulb->error" . PHP_EOL;
+
+} else echo " Лампа не доступна. Ошибка: $bulb->error" . PHP_EOL;
